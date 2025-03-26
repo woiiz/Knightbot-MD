3bda7e5bb1834d886576da70aad61bebeb33bbfd
woiiz <147359999+woiiz@users.noreply.github.com>
2025-03-27 00:27:06 +0800

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'W',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "WBot",
+  botOwner: 'woiiz', // Your name
+  ownerNumber: '601118502426', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
